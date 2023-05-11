59270d4da1e7140f2b0aed6b2b385d5f4293556a
xAsdfaf <georgejordanjr8@gmail.com>
2023-05-11 16:44:55 -0500

    maybe got the query page? not 100% sure.

---
+++ bookle/src/utils/query.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`
